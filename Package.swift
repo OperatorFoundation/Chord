94ef383494fb4c1b8b0e0e368df6f35265ff62d1
CryptoSax <joshua@operatorfoundation.org>
2021-11-08 13:32:29 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,7 +16,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(name: "Datable", url: "https://github.com/OperatorFoundation/Datable", from: "3.1.1"),
-        .package(url: "https://github.com/OperatorFoundation/SwiftQueue", from: "0.1.1"),
+        .package(url: "https://github.com/OperatorFoundation/SwiftQueue", from: "0.1.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
