ff1a564c73b7b2aef2f4a1f15d3c9f3af14ed06c
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2022-02-04 17:01:45 -0600

    Added FilteredQueue

---
+++ Sources/Chord/FilteredQueue.swift
@@ -0,0 +1,95 @@
+//
+//  FilteredQueue.swift
+//
+//
+//  Created by Dr. Brandon Wiley on 8/31/20.
+//
+
+import Foundation
+import SwiftQueue
+
+
+public class FilteredQueue<T>
+{
+    public static func drop(_ item: T)
+    {
+        return
+    }
+
+    var lock = DispatchGroup()
+    var filters: [Filter<T>] = []
+    var otherwise: (T) -> ()
+
+    public init(otherwise: @escaping (T) -> () = FilteredQueue.drop)
+    {
+        self.otherwise = otherwise
+    }
+
+    public func enqueue(element: T)
+    {
+        lock.enter()
+
+        for filter in filters
+        {
+            if filter.match(element)
+            {
+                filter.handle(element)
+
+                lock.leave()
+                return
+            }
+        }
+
+        otherwise(element)
+
+        lock.leave()
+    }
+
+    public func dequeue(match: @escaping (T) -> Bool, handle: @escaping (T) -> ()) -> T
+    {
+        let dequeueLock = DispatchGroup()
+        dequeueLock.enter()
+
+        var maybeResult: T? = nil
+
+        let handle =
+        {
+            (element: T) in
+
+            maybeResult = element
+            dequeueLock.leave()
+        }
+
+        self.addFilter(match: match, handle: handle)
+        dequeueLock.wait()
+
+        return maybeResult!
+    }
+
+    public func addFilter(match: @escaping (T) -> Bool, handle: @escaping (T) -> ())
+    {
+        let filter = Filter(match: match, handle: handle)
+        self.addFilter(filter)
+    }
+
+    public func addFilter(_ filter: Filter<T>)
+    {
+        lock.enter()
+
+        self.filters.append(filter)
+
+        lock.leave()
+    }
+}
+
+public struct Filter<T>
+{
+    let match: (T) -> Bool
+    let handle: (T) -> ()
+
+    public init(match: @escaping (T) -> Bool, handle: @escaping (T) -> ())
+    {
+        self.match = match
+        self.handle = handle
+    }
+}
