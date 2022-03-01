488a366f15278c53dd6bddacbb68c2a4a2853050
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2022-03-01 00:04:51 -0600

    Optional names for BlockingQueues

--- Sources/Chord/BlockingQueue.swift
+++ Sources/Chord/BlockingQueue.swift
@@ -14,38 +14,47 @@ public class BlockingQueue<T>: @unchecked Sendable
     var enqueueLock = DispatchSemaphore(value: 1)
     var dequeueLock = DispatchSemaphore(value: 0)
     let uuid = UUID()
+    let name: String
 
-    public init()
+    public init(name: String? = nil)
     {
+        if let name = name
+        {
+            self.name = name
+        }
+        else
+        {
+            self.name = self.uuid.uuidString
+        }
     }
     
     public func enqueue(element: T)
     {
-        print("BlockingQueue[\(self.uuid)].enqueue(\(element))")
-        print("BlockingQueue[\(self.uuid)].enqueue: waiting on enqueueLock")
+        print("BlockingQueue[\(self.name)].enqueue(\(element))")
+        print("BlockingQueue[\(self.name)].enqueue: waiting on enqueueLock")
         enqueueLock.wait()
-        print("BlockingQueue[\(self.uuid)].enqueue: passed enqueueLock")
+        print("BlockingQueue[\(self.name)].enqueue: passed enqueueLock")
 
         self.value = element
 
-        print("BlockingQueue[\(self.uuid)].enqueue: sending dequeueLock signal")
+        print("BlockingQueue[\(self.name)].enqueue: sending dequeueLock signal")
         dequeueLock.signal()
-        print("BlockingQueue[\(self.uuid)].enqueue: sent dequeueLock signal")
+        print("BlockingQueue[\(self.name)].enqueue: sent dequeueLock signal")
     }
     
     public func dequeue() -> T
     {
-        print("BlockingQueue[\(self.uuid)].dequeue()")
-        print("BlockingQueue[\(self.uuid)].dequeue: waiting on dequeueLock")
+        print("BlockingQueue[\(self.name)].dequeue()")
+        print("BlockingQueue[\(self.name)].dequeue: waiting on dequeueLock")
         dequeueLock.wait()
-        print("BlockingQueue[\(self.uuid)].dequeue: passed dequeueLock")
+        print("BlockingQueue[\(self.name)].dequeue: passed dequeueLock")
 
         let result = self.value!
         self.value = nil
 
-        print("BlockingQueue[\(self.uuid)].dequeue: sending enqueueLock signal")
+        print("BlockingQueue[\(self.name)].dequeue: sending enqueueLock signal")
         enqueueLock.signal()
-        print("BlockingQueue[\(self.uuid)].dequeue: sent enqueueLock signal")
+        print("BlockingQueue[\(self.name)].dequeue: sent enqueueLock signal")
 
         return result
     }
