413db0ac1a87ee766fff8d9f38cf4b367baff2c4
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2022-02-28 23:58:39 -0600

    Added debug logging

--- Sources/Chord/BlockingQueue.swift
+++ Sources/Chord/BlockingQueue.swift
@@ -13,6 +13,7 @@ public class BlockingQueue<T>: @unchecked Sendable
     var value: T? = nil
     var enqueueLock = DispatchSemaphore(value: 1)
     var dequeueLock = DispatchSemaphore(value: 0)
+    let uuid = UUID()
 
     public init()
     {
@@ -20,21 +21,31 @@ public class BlockingQueue<T>: @unchecked Sendable
     
     public func enqueue(element: T)
     {
+        print("BlockingQueue[\(self.uuid)].enqueue(\(element))")
+        print("BlockingQueue[\(self.uuid)].enqueue: waiting on enqueueLock")
         enqueueLock.wait()
+        print("BlockingQueue[\(self.uuid)].enqueue: passed enqueueLock")
 
         self.value = element
 
+        print("BlockingQueue[\(self.uuid)].enqueue: sending dequeueLock signal")
         dequeueLock.signal()
+        print("BlockingQueue[\(self.uuid)].enqueue: sent dequeueLock signal")
     }
     
     public func dequeue() -> T
     {
+        print("BlockingQueue[\(self.uuid)].dequeue()")
+        print("BlockingQueue[\(self.uuid)].dequeue: waiting on dequeueLock")
         dequeueLock.wait()
+        print("BlockingQueue[\(self.uuid)].dequeue: passed dequeueLock")
 
         let result = self.value!
         self.value = nil
 
+        print("BlockingQueue[\(self.uuid)].dequeue: sending enqueueLock signal")
         enqueueLock.signal()
+        print("BlockingQueue[\(self.uuid)].dequeue: sent enqueueLock signal")
 
         return result
     }
