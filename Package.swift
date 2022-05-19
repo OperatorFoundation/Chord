cfc0391d775a76535f9299ca6f6d1ce989b0ce27
Unseelie <emunseelie@gmail.com>
2022-05-19 13:18:52 -0500

    include package platform iOS, update depricated code

--- Package.swift
+++ Package.swift
@@ -1,11 +1,13 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Chord",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v10_15),
+                .iOS(.v15)
+               ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -15,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "Datable", url: "https://github.com/OperatorFoundation/Datable", branch: "main"),
+        .package(url: "https://github.com/OperatorFoundation/Datable", branch: "main"),
         .package(url: "https://github.com/OperatorFoundation/SwiftQueue", branch: "main"),
     ],
     targets: [
