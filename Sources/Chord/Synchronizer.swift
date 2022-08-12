7cd666ba5c6c44aa3d30e473abc5d50f1eb84c3a
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2022-08-12 15:07:05 -0500

    New synchronizer

--- Sources/Chord/Synchronizer.swift
+++ Sources/Chord/Synchronizer.swift
@@ -13,6 +13,7 @@ public typealias Caller<T> = (@escaping Callback<T>) -> Void
 public typealias Caller2<S, T> = (@escaping Callback2<S, T>) -> Void
 public typealias CallerThrows<T> = (@escaping Callback<T>) throws -> Void
 public typealias AsyncCaller<T> = () async -> T
+public typealias AsyncThrowingEffect<T> = () async throws -> Void
 
 // Convert a failable async function into a synchronous function with an optional return value
 public class Synchronizer
@@ -103,6 +104,38 @@ public class AsyncAwaitSynchronizer<T>
 
     let function: AsyncCaller<T>
     var result: T?
+
+    public init(_ function: @escaping AsyncCaller<T>)
+    {
+        self.function = function
+    }
+
+    func sync() -> T
+    {
+        let lock = DispatchSemaphore(value: 0)
+
+        async // Ignore warning about deprecation of async in favor of Task as Task does not currently work on Linux
+        {
+            self.result = await function()
+            lock.signal()
+        }
+
+        lock.wait()
+
+        return self.result!
+    }
+}
+
+@available(macOS 12.0, *)
+public class AsyncAwaitThrowingEffectSynchronizer
+{
+    static public func sync(_ function: @escaping AsyncThrowingEffect)
+    {
+        let synchronizer = AsyncAwaitThrowingEffectSynchronizer(function)
+        return synchronizer.sync()
+    }
+
+    let function: AsyncThrowingEffect
 
     public init(_ function: @escaping AsyncCaller<T>)
     {
