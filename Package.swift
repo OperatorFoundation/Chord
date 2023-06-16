086a9e628f2d63686ff9373ac76da6f79376eea3
consuelita <adelita@operatorfoundation.org>
2023-06-16 17:06:51 -0500

    Support iOS v16

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Chord",
     platforms: [.macOS(.v13),
-                .iOS(.v15)
+                .iOS(.v16)
                ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
